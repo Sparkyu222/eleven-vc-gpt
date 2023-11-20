d86ebefc8466f21e20f38fd6908e5e4873fd1f8e
Sparkyu222 <sparkyu222@gmail.com>
2023-11-20 19:24:58 +0400

    first commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# Eleven-vc-gpt
+
+I was bored and I tried to mix Eleven Labs's API with Open AI's one so I could make a personnal assitant with my voice. I'm still trying to learn Python, so the script might be a little bit janky.
+
+I only tested this script with Python 3.11.
+
+## Setting up
+First, copy the `.env-placeholder` file and rename it `.env`. In the copied file, you need to enter the following informations :
+```json
+{
+	"openai": "YOUR OPENAI API KEY HERE",
+	"elevenlabs": "YOUR ELEVENLABS API KEY HERE",
+	"voiceID": "THE ID OF THE VOICE OF YOUR CHOICE FROM ELEVENLABS"
+}
+```
+To obtain the ID of the voice you want to hear, [fetch the voices list from ElevenLabs's API](https://api.elevenlabs.io/docs#/voices/Get_voices_v1_voices_get).
+
+Then install the following requirements via `pip`:
+```bash
+$ pip install openai elevenslabs
+```
+And now, you're settled!
+
+## Run
+To start of your assistant, just launch the following script:
+
+```bash
+python main.py
+```
+
+This will initiate a basic turn-by-turn conversation with your assistant. After a few seconds — the time taken for ElevenLabs' API to infer and transfer the audio data — the voice of your choice will read ChatGPT's response.
---
+++ main.py
@@ -0,0 +1,73 @@
+from openai import OpenAI
+from elevenlabs import *
+import sys
+import json
+import threading
+
+
+
+## OPEN ".ENV" FILE TO GET API KEYS
+
+try:
+    envFile = open('.env', "r").read()
+except:
+    print('Error while trying to open .env file (did you copy the .env file?).')
+    exit(False)
+
+try:
+    env = json.loads(envFile)
+except:
+    print("Can't decode JSON data in .env file")
+    exit(False)
+
+
+
+## SETTING API KEYS
+
+client = OpenAI(
+    api_key=env["openai"]
+    )
+
+set_api_key(env["elevenlabs"])
+
+
+
+## FUNCTIONS DEFINITION
+
+def txtToSpeech(txt):
+    audio = generate(text=txt, voice=Voice(voice_id=env['voiceID']), model="eleven_multilingual_v2", stream=True)
+    stream(audio)
+
+
+
+## MAIN
+
+while True:
+
+    msg = input("Me : ")
+
+    completion = client.chat.completions.create(
+        model = "gpt-3.5-turbo",
+        messages = [
+            {"role": "system", "content": "You are a helpful assistant, you will only respond with short sentences"},
+            {"role": "user", "content": msg}
+        ],
+        temperature=0.8,
+        stream=True
+    )
+
+    output = ""
+
+    print("ChatGPT : ", end='')
+
+    for chunk in completion:
+        if chunk.choices[0].finish_reason == "stop":
+            print("")
+            continue
+
+        print(chunk.choices[0].delta.content, end='')
+        output += chunk.choices[0].delta.content
+        sys.stdout.flush()
+
+    thr = threading.Thread(target=txtToSpeech, args=(output,))
+    thr.start()
